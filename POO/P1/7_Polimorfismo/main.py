96c425dc26abadcbb8d3b671ec4aa3c3e92ec679
jesusVazquez21 <jesus_3141240253@utd.edu.mx>
2025-10-02 20:02:37 -0600

    Add files via upload

--- POO/P1/7_Polimorfismo/main.py
+++ POO/P1/7_Polimorfismo/main.py
@@ -1,101 +1,63 @@
-from Coches import *
+#Instanciar los objetos para posterior implementarlos 
+from coches import *
 import os
-#Solicitar los datos que posteriormente seran los atributs del objeto
 
+def datos_autos(tipo):
+    print(f"\n\t ...Ingresar los datos del Vehiculo de tipo: {tipo}")
+    marca=input("Marca: ").upper()
+    color=input("Color: ").upper()
+    modelo=input("Modelo: ").upper()
+    velocidad=int(input("Velocidad: "))
+    potencia=int(input("Potencia: "))
+    plazas=int(input("No. de plazas: "))
+    return marca,color,modelo,velocidad,potencia,plazas
+
+def imprimir_datos_vehiculo(marca,color,modelo,velocidad,potencia,plazas):
+    print(f"\n\tDatos del Vehiculo: \n Marca:{marca} \n color: {color} \n Modelo: {modelo} \n velocidad: {velocidad} \n caballaje: {potencia} \n plazas: {plazas}")
 
 def autos():
-    # num_coches = int(input("Cuantos coches tiene?"))
-    # for i in range(0,num_coches):
-    print("Datos del auto")
-    marca = input("Marca: ").upper()
-    color = input("Color: ").upper()
-    modelo = input("Modelo: ").upper()
-    velocidad = int(input("Velocidad: ").upper())
-    potencia = int(input("Potencia: ").upper())
-    plazas = int(input("Plazas: ").upper())
-    coche1 = Coches(marca,color,modelo,velocidad,potencia,plazas)
-        
-    coche1=Coches(marca, color, modelo, velocidad, potencia, plazas)
-        
-    print(f"Datos del Vehiculo: \n Marca:{coche1.marca()} \n color: {coche1.color()} \n Modelo: {coche1.modelo()} \n velocidad: {coche1.velocidad()} \n caballaje: {coche1.caballaje()} \n plazas: {coche1.plazas()} ")
+    marca,color,modelo,velocidad,potencia,plazas=datos_autos("Auto")
+    coche=Coches(marca,color,modelo,velocidad,potencia,plazas)
+    imprimir_datos_vehiculo(coche.marca,coche.color,coche.modelo,coche.velocidad,coche.caballaje,coche.plazas)
 
 def camionetas():
-    print("Datos del vehiculo")
-    marca = input("Marca: ").upper()
-    color = input("Color: ").upper()
-    modelo = input("Modelo: ").upper()
-    velocidad = int(input("Velocidad: ").upper())
-    potencia = int(input("Potencia: ").upper())
-    plazas = int(input("Plazas: ").upper())
-    traccion=input("Ingresar el tipo de traccion: ").upper()
-    cerrada=input("Ingresar SI/NO si es cerrada o no: ").upper()
+    marca,color,modelo,velocidad,potencia,plazas=datos_autos("Camioneta")
+    traccion=input("Traccion: ").upper()
+    cerrada=input("¿Cerrada (Si/No)?: ").upper().strip()
     if cerrada=="SI":
         cerrada=True
     else:
-        cerrada=False
-
-
-    coche1=Camionetas(marca, color, modelo, velocidad, potencia, plazas, traccion, cerrada)
-
-    print(f"Datos del Vehiculo: \n Marca:{coche1.marca()} \n color: {coche1.color()} \n Modelo: {coche1.modelo()} \n velocidad: {coche1.velocidad()} \n caballaje: {coche1.caballaje()} \n plazas: {coche1.plazas()} \n traccion: {coche1.traccion()} \n cerrada: {coche1.cerrada()} ")
+        cerrada=False    
+    coche=Camionetas(marca,color,modelo,velocidad,potencia,plazas,traccion,cerrada)
+    imprimir_datos_vehiculo(coche.marca,coche.color,coche.modelo,coche.velocidad,coche.caballaje,coche.plazas)
+    print(f"traccion: {coche.traccion}\n cerrada: {coche.cerrada}")
 
 def camiones():
-    print("Datos del camion")
-    marca = input("Marca: ").upper()
-    color = input("Color: ").upper()
-    modelo = input("Modelo: ").upper()
-    velocidad = int(input("Velocidad: ").upper())
-    potencia = int(input("Potencia: ").upper())
-    plazas = int(input("Plazas: ").upper())
-    eje=int(input("Ingresar la capacidad de carga: "))
-    capacidadCarga=int(input("Ingresar la capacidad de carga: "))
+    marca,color,modelo,velocidad,potencia,plazas=datos_autos("Camiones")
+    eje=int(input("No. de ejes: "))
+    capacidadCarga=int(input("Capacidad de carga: "))
+    coche=Camiones(marca,color,modelo,velocidad,potencia,plazas,eje,capacidadCarga)
+    imprimir_datos_vehiculo(coche.marca,coche.color,coche.modelo,coche.velocidad,coche.caballaje,coche.plazas)
+    print(f"#Ejes: {coche.eje}\n Capacidad de carga: {coche.capacidadCarga}")
 
 
-    coche1=Camionetas(marca, color, modelo, velocidad, potencia, plazas, eje, capacidadCarga)
-
-    print(f"Datos del Vehiculo: \n Marca:{coche1.marca()} \n color: {coche1.color()} \n Modelo: {coche1.modelo()} \n velocidad: {coche1.velocidad()} \n caballaje: {coche1.caballaje()} \n plazas: {coche1.plazas()} \n traccion: {coche1.eje()} \n cerrada: {coche1.capacidadCarga()} ")
-
-
-
-opcion=1
-while opcion!=4:
-    os.system("cls")
-    print("..:: MENU PRINCIPAL ::..")
-    opcion=input("\n\t 1.- Autos \n\t 2.- Camionetas\n\t 3.- Camiones\n\t 4.- Salir\n\t Elige una opción: ").lower().strip()
-    match opcion: 
-        case "1": 
+opcion=True
+while opcion:
+    os.system("clear")
+    opcion=input("\n\t\t ::: Menu Principal ::.\n\t1.- Autos\n\t2.-Camionetas\n\t3.-Camiones\n\t4.-Salir\n\tElige un opción: ").lower().strip()
+    match opcion:
+        case "1":
             autos()
             input("Oprima tecla para continuar")
         case "2":
             camionetas()
-            input("Oprima tecla para continuar")
+            input("Oprima tecla para continuar")  
         case "3":
             camiones()
             input("Oprima tecla para continuar")
         case "4":
-            print("Salir")
-            input("Oprima tecla para continuar")
+            input("Salir del Sistema")
+            opcion=False   
         case _:
-            print("Opción Invalida..... Vuelva a intentarlo")
-            input("Oprima tecla para continuar")  
+            input("Opcion invalidad ... vuelva a intertarlo ... ")      
 
-
-
-
-
-
-
-
-''' 
-num_coches = int(input("Cuantos coches tiene?"))
-for i in range(0,num_coches):
-    print("Datos del auto")
-    marca = input("Marca: ").upper()
-    color = input("Color: ").upper()
-    modelo = input("Modelo: ").upper()
-    velocidad = int(input("Velocidad: ").upper())
-    potencia = int(input("Potencia: ").upper())
-    plazas = int(input("Plazas: ").upper())
-    coche = Coches(marca,color,modelo,velocidad,potencia,plazas)
-
-'''
